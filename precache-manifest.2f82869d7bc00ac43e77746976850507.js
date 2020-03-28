cde294f95fabf9888c0f9d7faa70d6e5505457e3
Roberto <roberto71979@hotmail.com>
2020-03-27 20:04:45 -0400

    Updates

---
+++ precache-manifest.2f82869d7bc00ac43e77746976850507.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61441611588ec10bc517b4eb71e98f58",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "9de61ebf4ea5c3bc3bb5",
+    "url": "/personal-website/static/css/main.a17b6833.chunk.css"
+  },
+  {
+    "revision": "35999a3dfd6b9fbe69e8",
+    "url": "/personal-website/static/js/2.8dbad599.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/personal-website/static/js/2.8dbad599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de61ebf4ea5c3bc3bb5",
+    "url": "/personal-website/static/js/main.6137ab47.chunk.js"
+  },
+  {
+    "revision": "1e06780838e4eefcf678",
+    "url": "/personal-website/static/js/runtime-main.660c094e.js"
+  },
+  {
+    "revision": "a100879aaacc1d4ef26a7b5f78ff4ff4",
+    "url": "/personal-website/static/media/collegion.a100879a.svg"
+  },
+  {
+    "revision": "c6eb90955836aa5bdbcc6f8ea9467965",
+    "url": "/personal-website/static/media/logo.c6eb9095.png"
+  },
+  {
+    "revision": "97f3356e1512d57f13d5f640c029db03",
+    "url": "/personal-website/static/media/resellanalytics.97f3356e.svg"
+  },
+  {
+    "revision": "92a14c1185cf3620af62b52119329f55",
+    "url": "/personal-website/static/media/swiped.92a14c11.svg"
+  }
+]);
