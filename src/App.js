aaa9cbacb4423ebfed65c8115e57a6277b20e193
Infante <r.infante0407@gmail.com>
2022-01-05 17:50:40 -0500

    new website

--- src/App.js
+++ src/App.js
@@ -6,83 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <div className="line">
-            <h1>ROBERTO INFANTE <span>SOFTWARE DEVELOPER</span></h1>
-          </div>  
-
-           <div className="line">
-            <h1>ROBERTO INFANTE <span>SOFTWARE DEVELOPER</span></h1>
-          </div>   
-
-           <div className="line">
-            <h1>ROBERTO INFANTE <span>SOFTWARE DEVELOPER</span></h1>
-          </div>   
-
-           <div className="line">
-            <h1>ROBERTO INFANTE <span>SOFTWARE DEVELOPER</span></h1>
-          </div>   
-
-           <div className="line">
-            <h1>ROBERTO INFANTE <span>SOFTWARE DEVELOPER</span></h1>
-          </div>   
-
-           <div className="line">
-            <h1>ROBERTO INFANTE <span>SOFTWARE DEVELOPER</span></h1>
-          </div>      
-          
-
-          <div style={{marginTop: 120}} className="line">
-            <h1>ROBERTO INFANTE <span>SOFTWARE DEVELOPER</span></h1>
-          </div>   
-
-
-
-          <div>
-
-          <div style={{marginTop:-15}} className="small-line">
-            <a href="https://ternarydev.com" target="_blank">TERNARY DEVELOPMENTS / FOUNDER</a>
-
-            <a href="https:/github.com/Infante" target="_blank">GITHUB</a>
-
-          </div>  
-
-          <div className="small-line">
-            <a href="https://heed.chat" target="_blank">HEED / CTO</a>
-
-            <a href="https://www.linkedin.com/in/roberto-infante-bb4b56172/" target="_blank">LINKEDIN</a>
-
-          </div>  
-
-          <div className="small-line">
-            <a href="https://collegion.app" target="_blank">COLLEGION / FOUNDER</a>
-
-            <a href="mailto:r.infante0407@gmail.com">EMAIL</a>
-
-          </div>  
-
-          <div className="small-line">
-            <a href="https://resellanalytics.io" target="_blank">RESELLANALYTICS / PROJECT</a>
-            <a href="https://twitter.com/liluzi" target="_blank">TWITTER</a>
-          </div>  
-
-
-          <div className="small-line">
-            <a href="https://www.swiped.io" target="_blank">SWIPEDIO / PROJECT</a>
-          </div>  
-
-          <div className="small-line">
-            <a href="https://botbuddy.xyz" target="_blank">BOTBUDDYAIO / PROJECT</a>
-          </div>  
-
-
-          </div>
-
+        <div className="container"> 
+             
           <Track></Track>
 
-  
       
-        
         </div>
         
       </div>
--- src/components/Track.js
+++ src/components/Track.js
@@ -4,14 +4,18 @@ import { useLastFM } from 'use-last-fm';
 
 const CurrentlyPlaying = () => {
     const lastFM =  useLastFM('roberto_in7', '1af211e21f18e73f8d9eb3b6d5119c24')
+    console.log(lastFM);
     return (
         lastFM.status !== 'playing' ? (
-            <p style={{fontSize:12,position:'absolute', bottom:10, left:0, right:0}}></p>
+            <h1>ROBERTO INFANTE</h1>
         ) :  (
             <>
-            <p style={{fontSize:12,position:'absolute', bottom:10, left:0, right:0}}>
-                Currently Listening to {lastFM.song.name} by {lastFM.song.artist}...
-            </p>
+    
+                <h2>
+                    {lastFM.song.name.toUpperCase()}
+                </h2>
+    
+                <img  src={lastFM.song.art}></img>    
             </>
         )
     )
